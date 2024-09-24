580bb671e92aacc55b509779273d35aade152280
DominikKravec <dominik12123@gmail.com>
2024-09-24 15:39:02 +0200

    First commit.
    Create the project, setup native wind, create index.

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View className="justify-center items-center h-full ">
+      <Text className="text-blue-300 text-2xl">This is the health remider app.</Text>
+    </View>
+  )
+}
+
+export default index
