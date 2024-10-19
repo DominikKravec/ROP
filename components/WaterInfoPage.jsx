8b6d36d0b6f1ceaaa2581abedeede924b817f42d
DominikKravec <dominik12123@gmail.com>
2024-10-19 14:12:53 +0200

    Add circular progress

--- components/WaterInfoPage.jsx
+++ components/WaterInfoPage.jsx
@@ -5,6 +5,7 @@ import IconButton from './IconButton'
 import icons from '../constants/icons'
 import AddDrinkModal from './AddDrinkModal'
 import CustomModal from './CustomModal'
+import CircularProgress from 'react-native-circular-progress-indicator'
 
 const WaterInfoPage = () => {
 
@@ -13,40 +14,54 @@ const WaterInfoPage = () => {
     const [modal, setModal] = useState(false)
     const [modalContent, setModalContent] = useState(null)
 
+    const progress = waterDrank / waterGoal * 100
 
     return (
         <>
             <View className="w-[100vw] h-full justify-center items-center"> 
-                <Text className="text-2xl text-blue">
-                {/* change the height of the parent when you add more parts */}        
-                {waterDrank}/{waterGoal}{waterUnit}
-                </Text>
-                <View>
-                <View className="flex flex-row justify-between">
-                    <View className="w-[48%]">
-                    <IconButton
-                        icon={icons.cupIcon}
-                        title="quick cup"
+
+                <View className="justify-center items-center">
+                    <CircularProgress
+                        value={progress > 100 ? 100 : progress}
+                        inActiveStrokeColor={'#A5D9FF'}
+                        inActiveStrokeOpacity={0.2}
+                        activeStrokeColor={'#3CACFD'}
+                        progressValueColor={'#3CACFD'}
+                        valueSuffix={'%'}
+                    />
+
+                    <Text className="text-2xl text-blue mt-5">
+                        {/* change the height of the parent when you add more parts */}        
+                        {waterDrank}/{waterGoal}{waterUnit}
+                    </Text>
+                </View>
+
+                <View className="mt-5">
+                    <View className="flex flex-row justify-between">
+                        <View className="w-[48%]">
+                        <IconButton
+                            icon={icons.cupIcon}
+                            title="quick cup"
+                            handle={() => {setWaterDrank(waterDrank + cupVolume)}}
+                        />
+                        </View>
+
+                        <View className="w-[48%]">
+                        <IconButton
+                            icon={icons.plus}
+                            title="Add drink"
+                            handle={() => {setModal(true); setModalContent(<AddDrinkModal/>)}}
+                        />
+                        </View>
+                    </View>
+                    <View>
+                        <IconButton
+                        icon={icons.previous}
+                        title="Previous drink"
                         handle={() => {setWaterDrank(waterDrank + cupVolume)}}
-                    />
-                    </View>
+                        />
 
-                    <View className="w-[48%]">
-                    <IconButton
-                        icon={icons.plus}
-                        title="Add drink"
-                        handle={() => {setModal(true); setModalContent(<AddDrinkModal/>)}}
-                    />
                     </View>
-                </View>
-                <View>
-                    <IconButton
-                    icon={icons.previous}
-                    title="Previous drink"
-                    handle={() => {setWaterDrank(waterDrank + cupVolume)}}
-                    />
-
-                </View>
                 </View>
                 <CustomModal
                     modalContent={modalContent}
